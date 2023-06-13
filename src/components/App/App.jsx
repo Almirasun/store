f5be65c0baff1bc029804bb8370c7dab071c9b8d
Almirasun <almira.sagynbek@gmail.com>
2023-06-13 20:51:54 +0600

    initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AppRoutes from '../Routes/Routes';
+import '../../styles/App.css'
+
+const App = () => {
+    return (
+        <div className='app'>
+            <AppRoutes />
+        </div>
+    );
+}
+ 
+export default App;
