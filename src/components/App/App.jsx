74cd99c39265d1ff34bb911e4115e0e5c655db56
Almirasun <almira.sagynbek@gmail.com>
2023-06-17 21:05:04 +0600

    added dispatch & sidebar list

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import AppRoutes from '../Routes/Routes';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Sidebar from '../Sidebar/Sidebar';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import AppRoutes from "../Routes/Routes";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Sidebar from "../Sidebar/Sidebar";
+import { getCategories } from "../../features/categories/categoriesSlice";
 
 const App = () => {
-    return (
-        <div className='app'>
-            <Header />
-            
-            <div className="container">
-                <Sidebar />
-                <AppRoutes />
-            </div>
+  const dispatch = useDispatch();
 
-            <Footer />
-        </div>
-    );
-}
- 
+  useEffect(() => {
+    dispatch(getCategories())
+  }, [dispatch])
+
+  return (
+    <div className="app">
+      <Header />
+
+      <div className="container">
+        <Sidebar />
+        <AppRoutes />
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
 export default App;
