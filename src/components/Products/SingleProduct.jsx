9efcb4a620822e70041ac9af7cf8671724760723
Almirasun <almira.sagynbek@gmail.com>
2023-07-04 07:59:54 +0600

    added related products

--- src/components/Products/SingleProduct.jsx
+++ src/components/Products/SingleProduct.jsx
@@ -1,12 +1,19 @@
 import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { getRelatedProducts } from "../../features/products/productsSlice";
 import { useGetProductQuery } from "../../features/api/apiSlice";
 import { ROUTES } from "../../utils/routes";
+
 import Product from "./Product";
+import Products from "./Products";
 
 const SingleProduct = () => {
+  const dispatch = useDispatch();
   const { id } = useParams();
   const navigate = useNavigate();
+  const {related} = useSelector(({ products }) => products);
 
   const { data, isLoading, isFetching, isSuccess } = useGetProductQuery({ id });
   console.log(isLoading);
@@ -17,11 +24,19 @@ const SingleProduct = () => {
     }
   }, [isLoading, isFetching, isSuccess]);
 
+  useEffect(() => {
+    if (data) {
+      dispatch(getRelatedProducts(data.category.id));
+    }
+    console.log(data);
+  }, [data]);
+
   return !data ? (
     <section>Loading...</section>
   ) : (
     <>
       <Product {...data} />
+      <Products products={related} amount={5} title="Related Products" />
     </>
   );
 };
