ff41afe3233bb378a2ba1e38851fea21993dced9
Almirasun <almira.sagynbek@gmail.com>
2023-07-16 15:53:37 +0600

    added component for a category

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "../Home/Home";
+import { Route, Routes } from "react-router-dom";
 
-import {ROUTES} from "../../utils/routes"
+import { ROUTES } from "../../utils/routes";
+
+import Home from "../Home/Home";
 import SingleProduct from "../Products/SingleProduct";
 import Profile from "../Profile/Profile";
+import SingleCategory from "../Categories/SingleCategory";
 
 const AppRoutes = () => (
-    <Routes>
-        <Route index element={<Home />} />
-        <Route path={ROUTES.PRODUCT} element={<SingleProduct />} />
-        <Route path={ROUTES.PROFILE} element={<Profile />} />
-    </Routes>
+  <Routes>
+    <Route index element={<Home />} />
+    <Route path={ROUTES.PRODUCT} element={<SingleProduct />} />
+    <Route path={ROUTES.PROFILE} element={<Profile />} />
+    <Route path={ROUTES.CATEGORY} element={<SingleCategory />} />
+  </Routes>
 );
 
 export default AppRoutes;
